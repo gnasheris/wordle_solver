0fe193b4b2051f3d96f74b4423ab8a907bb60fc2
avlanaz <87753607+avlanaz@users.noreply.github.com>
2022-08-27 15:48:52 +1000

    Put prototype framework for solver on top of web page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <p>
+        How to input pattern:<br></br>
+        - Type letter word into its respective box<br></br>
+        - Put * (asterisk) for unknown letter<br></br>
+      </p>
+      <form action="GET" className="search-bar">
+        <label for="wordPattern">Word Pattern: </label>
+        <input type="text" placeholder="Search Wordle here..." name="word_pattern"></input>
+      </form>
+      <button className="search">Search Wordle</button>
+      <p>**print input here**</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
